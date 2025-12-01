2fc07950f6109b7f25ec80757943d3bfb87e0a15
Vedant <ucapvsm.ucl.ac.uk>
2025-12-01 11:14:11 +0000

    Extract Constant

--- boids/boids.py
+++ boids/boids.py
@@ -9,35 +9,17 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 
 import random
+import random
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+# --- Configuration ---
+BOID_COUNT = 50
+LIMIT_X_LOW, LIMIT_X_HIGH = -450, 50.0
+LIMIT_Y_LOW, LIMIT_Y_HIGH = 300.0, 600.0
+VELOCITY_X_LOW, VELOCITY_X_HIGH = 0, 10.0
+VELOCITY_Y_LOW, VELOCITY_Y_HIGH = -20.0, 20.0
 
-def update_boids(boids):
-    xs,ys,xvs,yvs=boids
-    # Fly towards the middle
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
-    # Fly away from nearby boids
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
-                xvs[i]=xvs[i]+(xs[i]-xs[j])
-                yvs[i]=yvs[i]+(ys[i]-ys[j])
-    # Try to match speed with nearby boids
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-                xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-                yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
-    # Move according to velocities
-    for i in range(len(xs)):
-        xs[i]=xs[i]+xvs[i]
-        ys[i]=ys[i]+yvs[i]
+COHESION_FACTOR = 0.01
+SEPARATION_DISTANCE_SQ = 100
+MATCH_SPEED_DISTANCE_SQ = 10000
+MATCH_SPEED_FACTOR = 0.125
+
